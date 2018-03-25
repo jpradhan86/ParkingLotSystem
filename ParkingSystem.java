960d977d017bc5165851650b2ccccee1022afd91
Jitendra Pradhan <jpradhan@Ban-1JPradhan-m.local>
2018-03-26 00:56:19 +0530

    Parking Lot invoker from file as well as stdin

---
+++ ParkingSystem.java
@@ -0,0 +1,48 @@
+package com.jitendra;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class ParkingSystem {
+	public void processThroughFile(String filePath) {
+    	ArrayList<String> commands = new ArrayList<String>();
+    	readCommandsFromFile(filePath, commands);
+    	ParkingLot parkingLot = new ParkingLot();
+        for (String command: commands) {
+            CommandBuilder.run_cmd(parkingLot, command);
+        }   
+    }
+    
+    public void processThroughUserInput() {
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ParkingLot parkingLot = new ParkingLot();
+        while(true) {
+            try {
+                    System.out.println("Input:");
+                    String cmd = reader.readLine();
+                    System.out.println("Output:");
+                CommandBuilder.run_cmd(parkingLot, cmd);
+            } catch (IOException e) {
+                System.err.println(e);
+            }   
+        }
+    }
+
+    public void readCommandsFromFile(String filename, ArrayList<String> commands) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line = reader.readLine();
+            while (line != null) {
+                //System.out.println(line);
+                commands.add(line);
+                line = reader.readLine();
+            }   
+            reader.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        }   
+    }
+}
